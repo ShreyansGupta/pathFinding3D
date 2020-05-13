0ac1dc3fc6dd46c27f0a1cc54836cfdc04b78fda
ShreyansGupta <50500026+ShreyansGupta@users.noreply.github.com>
2020-05-12 20:49:25 -0400

    Social Forces Updated

--- Assets/Scripts/SocialForces.cs
+++ Assets/Scripts/SocialForces.cs
@@ -6,27 +6,30 @@ public class SocialForces : MonoBehaviour
 {
     private Rigidbody rb;
     private float perceptionRadius;
-    private PathFindingAgent pathFinding;
+    private Dictionary<GameObject, Vector3> perceivedNeighbors = new Dictionary<GameObject, Vector3>();
+
     // Start is called before the first frame update
     void Start()
     {
-        pathFinding = GetComponent<PathFindingAgent>();
         rb = GetComponent<Rigidbody>();
-        perceptionRadius=GetComponent<SphereCollider>().radius;
+        perceptionRadius = GetComponent<SphereCollider>().radius;
     }
 
     private void Update()
     {
-        computeForce();
+        if (perceivedNeighbors.Count!=0)
+            computeForce();
     }
 
     private void computeForce()
     {
         var force = Vector3.zero;
-        var perceivedNeighbors = pathFinding.perceivedNeighbors;
+        
         foreach (var obj in perceivedNeighbors)
         {
             force += calculateWallForce(obj.Key) * 0.0001f;
+            
+            
         }
         rb.AddForce(force * 10, ForceMode.Force);
     }
@@ -35,7 +38,7 @@ public class SocialForces : MonoBehaviour
     {
         var wallForce = Vector3.zero;
 
-        var normal = transform.position - pathFinding.perceivedNeighbors[wall];
+        var normal = transform.position - perceivedNeighbors[wall];
         if (Mathf.Abs(normal.x) > Mathf.Abs(normal.z))
         {
             normal.z = 0;
@@ -45,18 +48,36 @@ public class SocialForces : MonoBehaviour
             normal.x = 0;
         }
         normal = normal.normalized;
-        
+
         normal.y = 0;
-        Debug.DrawRay(pathFinding.perceivedNeighbors[wall], normal*100,Color.red);
-        var dir = transform.position - pathFinding.perceivedNeighbors[wall];
+        Debug.DrawRay(perceivedNeighbors[wall], normal * 100, Color.red);
+        var dir = transform.position - perceivedNeighbors[wall];
         dir.y = 0;
         var projection = Vector3.Project(dir, normal);
 
         var exponent = Mathf.Exp(((perceptionRadius + 0.5f) - projection.magnitude) / 100f);
-        wallForce += (2000f * exponent) * normal*50;
+        wallForce += (2000f * exponent) * normal * 250;
         //Debug.Log("Force" + wallForce.magnitude);
 
         return wallForce;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "SideWall" && !perceivedNeighbors.ContainsKey(other.gameObject))
+        {
+            //Debug.Log(other.gameObject.name);
+            Vector3 poc = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+            perceivedNeighbors.Add(other.gameObject, poc);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (perceivedNeighbors.ContainsKey(other.gameObject))
+        {
+            perceivedNeighbors.Remove(other.gameObject);
+        }
     }
 
 }
