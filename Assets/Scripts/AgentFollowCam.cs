c3faa0a5e5be166697b28f5ef3c5191eb0a57ae7
kartk <kaarthikgovindappa@gmail.com>
2020-05-13 20:13:55 -0400

    Follow cam damping

--- Assets/Scripts/AgentFollowCam.cs
+++ Assets/Scripts/AgentFollowCam.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class AgentFollowCam : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float damping;
+    
     void Update()
     {
         var name = "";
@@ -53,6 +48,7 @@ public class AgentFollowCam : MonoBehaviour
                 fc.enabled = false;
                 if (fc.gameObject != gameObject && fc.gameObject.name == name)
                 {
+                    fc.damping = damping;
                     fc.enabled = true;
                 }
             }
